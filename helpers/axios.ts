2ddca7e2b1c6c1ba37aefd1dafe5ae86d9de34fd
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2026-06-04 12:12:31 +0100

    Edits

--- helpers/axios.ts
+++ helpers/axios.ts
@@ -1,57 +1,57 @@
-import { globals } from "@/lib/constants";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { create } from "axios";
-import * as Sentry from "@sentry/react-native";
+  import { globals } from "@/lib/constants";
+  import AsyncStorage from "@react-native-async-storage/async-storage";
+  import { create } from "axios";
+  import * as Sentry from "@sentry/react-native";
 
-const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL
+  const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL
 
-// api constructor
-const api = create({
-  baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-  timeout: 30000,
-});
-
-// interceptor
-let requestInterceptor: number;
-let responseInterceptor: number;
-
-export const setupInterceptors = (logout: () => void) => {
-  if (requestInterceptor !== undefined) {
-    api.interceptors.request.eject(requestInterceptor);
-  }
-
-  if (responseInterceptor !== undefined) {
-    api.interceptors.response.eject(responseInterceptor);
-  }
-
-  requestInterceptor = api.interceptors.request.use(async (config) => {
-    const token = await AsyncStorage.getItem(globals.AUTH_TOKEN_KEY);
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
+  // api constructor
+  const api = create({
+    baseURL: API_BASE_URL,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    timeout: 30000,
   });
 
-  responseInterceptor = api.interceptors.response.use(
-    (res) => res,
-    async (error) => {
-      Sentry.captureException(error);
-      if (error.code === "ERR_NETWORK") {
-        console.error("No internet or server down");
-      } else if (error.response?.status === 401) {
-        logout();
-        throw new Error("Session expired. Logging out...");
+  // interceptor
+  let requestInterceptor: number;
+  let responseInterceptor: number;
+
+  export const setupInterceptors = (logout: () => void) => {
+    if (requestInterceptor !== undefined) {
+      api.interceptors.request.eject(requestInterceptor);
+    }
+
+    if (responseInterceptor !== undefined) {
+      api.interceptors.response.eject(responseInterceptor);
+    }
+
+    requestInterceptor = api.interceptors.request.use(async (config) => {
+      const token = await AsyncStorage.getItem(globals.AUTH_TOKEN_KEY);
+
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
       }
 
-      return Promise.reject(error);
-    },
-  );
-};
+      return config;
+    });
 
-export default api;
+    responseInterceptor = api.interceptors.response.use(
+      (res) => res,
+      async (error) => {
+        Sentry.captureException(error);
+        if (error.code === "ERR_NETWORK") {
+          console.error("No internet or server down");
+        } else if (error.response?.status === 401) {
+          logout();
+          throw new Error("Session expired. Logging out...");
+        }
+
+        return Promise.reject(error);
+      },
+    );
+  };
+
+  export default api;
